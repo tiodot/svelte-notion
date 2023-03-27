ae5884d86ae6ded0d9500ab5116649db1f94ebaf
tiodot <tiodot@163.com>
2023-03-27 17:51:23 +0800

    add style

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,6 @@
 <script>
+	import 'prismjs/themes/prism-coy.css';
+	// from react-notion-x
 	// import './styles.css';
 	import './notion.css';
 	import './prism-theme.css';
@@ -14,5 +16,4 @@
 		flex-direction: column;
 		min-height: 100vh;
 	}
-
 </style>
